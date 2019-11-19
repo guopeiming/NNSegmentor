54a187d4b87735063c7452457a11f04d75edeb0f
guopeiming <1072671422@qq.com>
2019-11-19 21:10:31 +0800

    adjust loading pretrained embeddings method

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -3,9 +3,7 @@
 # @File : train.py
 # @Last Modify Time : 2019/10/18 08:33
 # @Contact : 1072671422@qq.com, guopeiming2016@{gmail.com, 163.com}
-import sys
 import torch
-import numpy as np
 import torch.nn as nn
 import torch.nn.init as init
 from config import Constants
@@ -13,80 +11,60 @@ from torch.utils.data import DataLoader
 from utils.dataset import CWSDataset, pad_collate_fn
 
 
-def gen_embed_nnembed(embeddings_dic, id2item, length):
-    oov_num = 0
-    embed = nn.Embedding(len(id2item), length, padding_idx=Constants.padId).weight
-    init.xavier_uniform_(embed)
-    embed = embed.numpy()
-    for idx, item in enumerate(id2item):
-        if item in embeddings_dic:
-            embed[idx] = np.array(embeddings_dic[item], dtype=np.float32)
+def build_embed_tensor_from_file(embeddings, filename, vocab):
+    embed_size = embeddings.shape[1]
+    hit_set = set()
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            cont_list = line.strip().split(' ')
+            if cont_list[0] in vocab:
+                assert embed_size == len(cont_list) - 1, 'pretrained embeddings size error.'
+                embeddings[vocab[cont_list[0]]] = torch.tensor([x for x in cont_list[1:]])
+                hit_set.add(cont_list[0])
+    return embeddings, hit_set
+
+
+def get_pretr_embed_tensor(flag, filename, vocab, embed_dim, gen_oov_mode, gen_oov_uniform, max_norm, typ):
+    print('Loading %s embeddings starts...' % typ)
+    if flag:
+        assert gen_oov_mode in ['zeros', 'nnembed', 'avg', 'uniform', 'randn'], \
+            'Mode of generating oov embedding vector does not exist.'
+        if gen_oov_mode == 'nnembed':
+            embeddings = torch.nn.Embedding(len(vocab), embed_dim, Constants.padId, max_norm).weight
+        elif gen_oov_mode == 'uniform':
+            embeddings = init.uniform_(torch.randn((len(vocab), embed_dim)), -gen_oov_uniform, gen_oov_uniform)
+        elif gen_oov_mode == 'randn':
+            embeddings = torch.randn((len(vocab), embed_dim))
         else:
-            oov_num += 1
-    assert oov_num < Constants.TEST_OOV_NUM, 'The number of oov is too big.'
-    print('The number of (oov) is %d' % oov_num)
-    return torch.tensor(embed)
-
-
-def read_embed_file(id2item, filename, length, gen_oov_mode, uniform_par):
-    assert gen_oov_mode in ['zeros', 'nnembed', 'avg', 'uniform'], \
-        'Mode of generating oov embedding vector does not exist.'
-    embeddings_dic = {}
-    with open(filename, mode='r', encoding='utf-8') as reader:
-        lines = reader.readlines()
-        assert length == len(lines[0].strip().split(' ')) - 1, 'Pretrained embeddings dimension is correct.'
-        for line in lines:
-            strs = line.strip().split(' ')
-            if strs[0] in id2item:
-                embeddings_dic[strs[0]] = [float(value) for value in strs[1:]]
-    if Constants.padKey not in embeddings_dic:
-        embeddings_dic[Constants.padKey] = [0.] * length
-        print('pretrained embeddings does not include <pad>, init it zeros default.')
-    if gen_oov_mode == 'nnembed':
-        return gen_embed_nnembed(embeddings_dic, id2item, length)
+            embeddings = torch.zeros((len(vocab), embed_dim))
+        print('Loading %s pretrained embeddings from %s' % (typ, filename))
+        embeddings, hit_set = build_embed_tensor_from_file(embeddings, filename, vocab)
+        print('%d %s are hit in pretrained embeddings file' % (len(hit_set), typ))
+        print('%d %s are OOV, which are inited by %s' % ((len(vocab) - len(hit_set)), typ, gen_oov_mode))
+        print('OOV %s:' % typ)
+        oov_set = set(vocab.keys()).difference(hit_set)
+        print(oov_set)
+        assert (len(vocab) - len(hit_set)) <= Constants.MAX_OOV_NUM, 'OOV num too big.'
+        if gen_oov_mode == 'avg':
+            avg_tensor = torch.mean(embeddings, 0)
+            for key in oov_set:
+                embeddings[vocab[key]] = avg_tensor.clone().detach()
     else:
-        avg = np.mean(np.array([embeddings_dic[key] for key in embeddings_dic], dtype=np.float32), axis=0)
-        embed = np.empty((len(id2item), length), dtype=np.float32)
-        oov_num = 0
-        for idx, item in enumerate(id2item):
-            if item in embeddings_dic:
-                embed[idx] = np.array(embeddings_dic[item], dtype=np.float32)
-            else:
-                if gen_oov_mode == 'zeros':
-                    embed[idx] = np.array([0.] * length, dtype=np.float32)
-                elif gen_oov_mode == 'uniform':
-                    embed[idx] = np.random.uniform(-uniform_par, uniform_par, size=length)
-                else:
-                    embed[idx] = avg
-                oov_num += 1
-                print('pretrained embeddings does not include %s, init it by %s default.' % (item, gen_oov_mode))
-                sys.stdout.flush()
-        assert oov_num < Constants.MAX_OOV_NUM, 'The number of oov is too big.'
-        print('The number of (oov) is %d' % oov_num)
-        return torch.tensor(embed)
+        embeddings = init.xavier_uniform_(nn.Embedding(len(vocab), embed_dim, Constants.padId, max_norm).weight)
+        print('%s pretrained embeddings was loaded by random.' % typ)
+    print('Loading %s embeddings ends.\n' % typ)
+    return embeddings
 
 
 def load_pretrained_embeddings(train_dataset, config):
-    print('Loading char embeddings starts...')
-    if config.pretrained_embed_char:
-        print('Loading char pretrained embeddings from %s' % config.pretrained_char_embed_file)
-        char_embeddings = read_embed_file(train_dataset.get_id2char(), config.pretrained_char_embed_file,
-                                          config.char_embed_dim, config.char_gen_oov_mode, config.char_gen_oov_uniform)
-    else:
-        char_embeddings = init.xavier_uniform_(nn.Embedding(train_dataset.get_char_vocab_size(), config.char_embed_dim,
-                                                            padding_idx=Constants.padId, max_norm=config.char_embed_max_norm).weight)
-        print('char pretrained embeddings was loaded by random.')
-    print('Loading char embeddings ends.\n')
-    sys.stdout.flush()
-    print('Loading bichar embeddings starts...')
-    if config.pretrained_embed_bichar:
-        print('Loading bichar pretrained embeddings from %s' % config.pretrained_bichar_embed_file)
-        bichar_embeddings = read_embed_file(train_dataset.get_id2bichar(), config.pretrained_bichar_embed_file,
-                                            config.bichar_embed_dim, config.bichar_gen_oov_mode, config.bichar_gen_oov_uniform)
-    else:
-        bichar_embeddings = init.xavier_uniform_(nn.Embedding(train_dataset.get_bichar_vocab_size(), config.bichar_embed_dim,
-                                                            padding_idx=Constants.padId, max_norm=config.bichar_embed_max_norm).weight)
-    print('Loading bichar embeddings ends.\n')
+    char_embeddings = get_pretr_embed_tensor(config.pretrained_embed_char, config.pretrained_char_embed_file,
+                                             train_dataset.get_char2id(), config.char_embed_dim,
+                                             config.char_gen_oov_mode, config.char_gen_oov_uniform,
+                                             config.char_embed_max_norm, 'char')
+    bichar_embeddings = get_pretr_embed_tensor(config.pretrained_embed_bichar, config.pretrained_bichar_embed_file,
+                                               train_dataset.get_bichar2id(), config.bichar_embed_dim,
+                                               config.bichar_gen_oov_mode, config.bichar_gen_oov_uniform,
+                                               config.bichar_embed_max_norm, 'bichar')
     return char_embeddings, bichar_embeddings
 
 
