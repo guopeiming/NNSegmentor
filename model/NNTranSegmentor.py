1c032bfb48c3d3bdc6a4bde7a98e0f0b3580dfbb
guopeiming <1072671422@qq.com>
2019-11-25 21:47:10 +0800

    fix a bug

--- model/NNTranSegmentor.py
+++ model/NNTranSegmentor.py
@@ -1,27 +1,65 @@
 # @Author : guopeiming
-# @Datetime : 2019/10/16 14:17
+# @Datetime : 2019/11/14 20:13
 # @File : dataset.py
-# @Last Modify Time : 2019/10/18 08:33
+# @Last Modify Time : 2019/11/14 20:13
 # @Contact : 1072671422@qq.com, guopeiming2016@{gmail.com, 163.com}
 
 
+import torch
 import torch.nn as nn
 from model.char_encoder import CharEncoder
-from model.word_decoder import WordDecoder
+from model.StackLSTMCell import StackLSTMCell
+from model.SubwordLSTMCell import SubwordLSTMCell
 
 
-class NNTranSegmentor(nn.Module):
+class ParaNNTranSegmentor(nn.Module):
     """
-    NNTranSegmentor
+    ParaNNTranSegmentor
     """
 
-    def __init__(self, id2char, word2id, char_vocab_size, word_vocab_size, config):
-        super(NNTranSegmentor, self).__init__()
+    def __init__(self, pretra_char_embed, char_embed_num, char_embed_dim, char_embed_max_norm, pretra_bichar_embed,
+                 bichar_embed_num, bichar_embed_dim, bichar_embed_max_norm, dropout_embed, encoder_embed_dim,
+                 dropout_encoder_embed, encoder_lstm_hid_size, dropout_encoder_hid, subword_lstm_hid_size,
+                 word_lstm_hid_size, device):
+        super(ParaNNTranSegmentor, self).__init__()
 
-        self.char_encoder = CharEncoder(char_vocab_size, id2char, config)
-        self.word_decoder = WordDecoder(word_vocab_size, id2char, word2id, config)
+        self.char_encoder = CharEncoder(pretra_char_embed, char_embed_num, char_embed_dim, char_embed_max_norm,
+                                        pretra_bichar_embed, bichar_embed_num, bichar_embed_dim, bichar_embed_max_norm,
+                                        dropout_embed, encoder_embed_dim, dropout_encoder_embed, encoder_lstm_hid_size,
+                                        dropout_encoder_hid, device)
+        self.subwStackLSTM = SubwordLSTMCell(encoder_lstm_hid_size*2, subword_lstm_hid_size, device)
+        self.wordStackLSTM = StackLSTMCell(2*subword_lstm_hid_size, word_lstm_hid_size, device)
+        self.classifier = nn.Linear(word_lstm_hid_size+2*encoder_lstm_hid_size, 2, bias=True)
+        self.device = device
+        self.subword_action_map = torch.tensor([1, 2, 2]).to(self.device)
+        self.word_action_map = torch.tensor([0, 1, 0]).to(self.device)
+        self.__init_para()
 
     def forward(self, insts, golds=None):
-        char_context = self.char_encoder(insts)  # [char_num, batch, embeddings_dim]
-        pred = self.word_decoder(char_context, insts, golds)  # [batch, char_num, label_num]
-        return pred
+        chars = self.char_encoder(insts)  # (seq_len, batch_size, encoder_lstm_hid_size*2)
+
+        batch_size, seq_len = insts[0].shape[0], insts[0].shape[1]
+        self.subwStackLSTM.init_stack(batch_size)
+        self.wordStackLSTM.init_stack(batch_size)
+
+        pred = [torch.tensor([[[-1., 1.]]]).expand((batch_size, 1, 2)).to(self.device)]
+        for idx in range(1, seq_len, 1):
+            subword, sub_h, sub_c = self.subwStackLSTM(chars[idx - 1, :, :])  # (batch_size, word_lstm_hid_size)
+            word_repre, word_c = self.wordStackLSTM(subword)  # (batch_size, word_lstm_hid_size)
+            output = self.classifier(torch.cat([word_repre, chars[idx, :, :]], 1))  # (batch_size, 2)
+
+            if self.training:
+                subwordOP = self.subword_action_map.index_select(0, golds[:, idx])
+                wordOP = self.word_action_map.index_select(0, golds[:, idx])
+            else:
+                subwordOP = self.subword_action_map.index_select(0, torch.argmax(output, 1))
+                wordOP = self.word_action_map.index_select(0, torch.argmax(output, 1))
+            self.subwStackLSTM.update_pos(subwordOP, sub_h, sub_c)
+            self.wordStackLSTM.update_pos(wordOP, word_repre, word_c)
+
+            pred.append(output.unsqueeze(1))
+        return torch.cat(pred, 1)  # (batch_size, seq_len, 2)
+
+    def __init_para(self):
+        nn.init.xavier_uniform_(self.classifier.weight)
+        nn.init.uniform_(self.classifier.bias)
--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from utils.model_utils import load_data
 from utils.visualLogger import VisualLogger
 from utils.model_utils import get_lr_scheduler_lambda
 from utils.model_utils import load_pretrained_embeddings
-from model.ParaNNTranSegmentor import ParaNNTranSegmentor
+from model.NNTranSegmentor import ParaNNTranSegmentor
 
 
 def parse_args():
@@ -134,7 +134,6 @@ def main():
 
     criterion = torch.nn.CrossEntropyLoss(reduction='sum').to(config.device)
     optimizer = Optim(config.opti_name, config.learning_rate, config.weight_decay, model)
-    scheduler = lr_scheduler.LambdaLR(optimizer.get_optimizer(), get_lr_scheduler_lambda(config.warmup_steps, config.lr_decay_factor))
     visual_logger = VisualLogger(config.visual_logger_path)
 
     # ========= Training ========= #
@@ -162,7 +161,6 @@ def main():
             if config.clip_grad:
                 utils.clip_grad_norm_(model.parameters(), config.clip_grad_max_norm)
             optimizer.step()
-            scheduler.step()
 
             if steps % config.logInterval == 0:
                 avg_loss = total_loss/chars
@@ -173,7 +171,7 @@ def main():
                 print('[%d/%d], [%d/%d] Loss: %.05f, F: %.05f, P: %.05f, R: %.05f, ACC: %.05f' %
                       (epoch_i+1, config.epoch, batch_i+1, len(train_data), avg_loss, F, P, R, ACC))
                 sys.stdout.flush()
-                scal = {'Loss': avg_loss, 'F': F, 'P': P, 'R': R, 'ACC': ACC, 'rl': scheduler.get_lr()[0]}
+                scal = {'Loss': avg_loss, 'F': F, 'P': P, 'R': R, 'ACC': ACC}
                 visual_logger.visual_scalars(scal, steps, 'train')
                 total_loss, golds_words, pred_words, seg_words, chars, cor_chars = 0.0, 0, 0, 0, 0, 0
                 # break
