ac91f23135f12bbb8f35969e2a8c8441be0c1e75
guopeiming <1072671422@qq.com>
2019-11-28 21:33:44 +0800

    fix SEP

--- config/Constants.py
+++ config/Constants.py
@@ -1,8 +1,8 @@
 # @Author : guopeiming
 # @Datetime : 2019/10/15 21:07
-# @File : config.py
-# @Last Modify Time : 2019/10/15 21:07
-# @Contact : 1072671422@qq.com, guopeiming2016@{gmail.com, 163.com}
+# @File : Constants.py
+# @Last Modify Time : 2019/11/28 21:07
+# @Contact : guopeiming2016@{qq, gmail, 163}.com
 oovKey = '<unk>'
 oovId = 0
 padKey = '<pad>'
@@ -10,7 +10,7 @@ padId = 1
 BOS = '<BOS>'
 EOS = '<EOS>'
 APP = 0
-SEP = 1
+SEG = 1
 actionPadId = 2
 embeddings_layer_keyword = 'embeddings'
 EPSILON = 1e-10
