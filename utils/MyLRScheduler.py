ca635640b2a63c818aa6ec796426418e4e829ee7
guopeiming <1072671422@qq.com>
2019-12-29 20:36:24 +0800

    fix a bug of scheduler

--- utils/MyLRScheduler.py
+++ utils/MyLRScheduler.py
@@ -1,8 +1,7 @@
 # @Author : guopeiming
-# @Datetime : 2019/11/26 14:22
-# @File : train.py
-# @Last Modify Time : 2019/11/26 14:22
 # @Contact : guopeiming2016@{qq.com, gmail.com, 163.com}
+from typing import List
+from torch.optim.optimizer import Optimizer
 from torch.optim.lr_scheduler import LambdaLR
 
 
@@ -11,9 +10,9 @@ class MyLRScheduler(LambdaLR):
     My lr scheduler class, it can convert to warm_up, no_warm_up, constant, warm_up_reduce learning rate mode
     by parameters.
     """
-    def __init__(self, optimizer, lr_lambda, last_epoch=-1):
+    def __init__(self, optimizer: Optimizer, lr_lambdas: List, last_epoch=-1):
         self.curr_lrs = list(map(lambda group: group['lr'], optimizer.param_groups))
-        super(MyLRScheduler, self).__init__(optimizer, lr_lambda, last_epoch)
+        super(MyLRScheduler, self).__init__(optimizer, lr_lambdas, last_epoch)
 
     def get_lr(self):
         self.curr_lrs = [lmbda(self.last_epoch, lr) for lmbda, lr in zip(self.lr_lambdas, self.curr_lrs)]
--- utils/optim.py
+++ utils/optim.py
@@ -2,6 +2,7 @@
 # @Contact : guopeiming2016@{qq, gmail, 163}.com
 import torch.optim
 import torch.nn.utils as utils
+from config.config import MyConf
 from collections.abc import Iterable
 from config.Constants import bertAttr
 from model.BertCharSegmentor import BertCharSegmentor
@@ -14,23 +15,29 @@ class Optim:
     """
     name_list = ['Adam', 'SGD']
 
-    def __init__(self, optim_name, learning_rate, fine_tune_lr, weight_decay, model: BertCharSegmentor, config):
-        assert optim_name in Optim.name_list, 'optimizer name is wrong.'
+    def __init__(self, model: BertCharSegmentor, config: MyConf):
+        assert config.opti_name in Optim.name_list, 'optimizer name is wrong.'
 
         params_list = []
+        lr_lambdas = []
         for name, child in model.named_children():
-            params_list.append({'params': child.parameters(), 'lr': fine_tune_lr if bertAttr in name else learning_rate})
+            if bertAttr in name:
+                params_list.append({'params': child.parameters(), 'lr': config.fine_tune_lr})
+                lr_lambdas.append(get_lr_scheduler_lambda(config.fine_tune_lr, config.warmup_steps, config.lr_decay_factor))
+            else:
+                params_list.append({'params': child.parameters(), 'lr': config.learning_rate})
+                lr_lambdas.append(get_lr_scheduler_lambda(config.learning_rate, config.warmup_steps, config.lr_decay_factor))
 
-        if optim_name == 'Adam':
-            self._optimizer = torch.optim.Adam(params_list, weight_decay=weight_decay)
-        if optim_name == 'SGD':
-            self._optimizer = torch.optim.SGD(params_list, learning_rate, config.momentum, config.dampening,
-                                              weight_decay, config.nesterov)
+        if config.opti_name == 'Adam':
+            self._optimizer = torch.optim.Adam(params_list, weight_decay=config.weight_decay)
+        if config.opti_name == 'SGD':
+            self._optimizer = torch.optim.SGD(params_list, config.learning_rate, config.momentum, config.dampening,
+                                              config.weight_decay, config.nesterov)
 
         self.model = model
         self.clip_grad = config.clip_grad
         self.clip_grad_max_norm = config.clip_grad_max_norm
-        self.scheduler = MyLRScheduler(self._optimizer, get_lr_scheduler_lambda(learning_rate, config.warmup_steps, config.lr_decay_factor))
+        self.scheduler = MyLRScheduler(self._optimizer, lr_lambdas)
 
     def zero_grad(self):
         self._optimizer.zero_grad()
