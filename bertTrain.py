63f6fd20b5835b4690486610f81bb0be134004cb
guopeiming <1072671422@qq.com>
2020-01-03 14:57:25 +0800

    delete empty line

--- bertTrain.py
+++ bertTrain.py
@@ -73,7 +73,6 @@ def eval_model(model, criterion, dev_data, test_data, device, visual_logger, sta
 def eval_dataset(model, criterion, data, device, typ, visual_logger, stamp):
     total_loss, golds_words, pred_words, seg_words, chars, cor_chars = 0.0, 0, 0, 0, 0, 0
     for insts, golds in data:
-        # torch.cuda.empty_cache()
         golds = golds.to(device)
 
         pred = model(insts, golds)
